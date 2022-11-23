f3b76d8471a8b027263ca5b524047a3acb513ff4
Lotfi Jbeli <lotfijbeli.jbeli@gmail.com>
2022-11-23 17:37:33 +0100

    Styles update

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 const Note = (props) => {
   return (
-    <div className="note" onClick={props.handleShow}>
+    <div className="note note-item" onClick={props.handleShow}>
       <h3 className="note-title">{props.title.substring(0, 40)}</h3>
       <span className="note-text">{props.text.substring(0, 40)}</span>
       <div className="footer--note">
         <small>{props.date}</small>
         <span className="delete-icon-container" onClick={props.handleDelete}>
-          <DeleteIcon style={{ color: "red" }} />
+          <DeleteIcon style={{ color: "black" }} />
         </span>
       </div>
     </div>
